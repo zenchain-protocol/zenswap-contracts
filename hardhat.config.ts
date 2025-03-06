f19fc0fe8e1bb4ee7025700d74a692139a67ae97
Ben Walker <ben.walker18@gmail.com>
2025-03-05 17:20:03 -0800

    WIP - deploy script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,39 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.28", // Mock Contracts
+        settings: {
+          optimizer: { enabled: true, runs: 200 },
+        },
+      },
+      {
+        version: "0.6.6", // Uniswap V2 Periphery
+        settings: {
+          optimizer: { enabled: true, runs: 200 },
+        },
+      },
+      {
+        version: "0.5.16", // Uniswap V2 Core
+        settings: {
+          optimizer: { enabled: true, runs: 200 },
+        },
+      },
+    ],
+  },
+  paths: {
+    sources: "./contracts",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  networks: {
+    hardhat: {
+      loggingEnabled: true,
+      chainId: 31337,
+    },
+  },
 };
 
 export default config;
